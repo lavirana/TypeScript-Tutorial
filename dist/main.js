1272d6ca025e662ca59a5daaabb79abe7f20b690
Ashish Rana <ashishrana288@gmail.com>
2026-03-23 16:01:47 +0530

    added typescript basic types

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,24 @@
-"use strict";
 let firstname = "Ashish";
 console.log(firstname);
+let someVariable;
+someVariable = 12;
+someVariable = "hello";
+//unknown
+let somevar;
+somevar = 12;
+//normal function
+function add(a, b) {
+    return a + b;
+}
+console.log(add(1, 5));
+//function expression
+const add2 = function (a, b) {
+    return a + b;
+};
+console.log(add2(4, 8));
+//arrow function
+const add3 = (a, b) => {
+    return a + b;
+};
+console.log(add3(4, 6));
+export {};
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,28 @@
 let firstname: string = "Ashish";
 console.log(firstname);
+
+let someVariable: any;
+someVariable = 12;
+someVariable = "hello";
+
+//unknown
+let somevar: unknown;
+somevar = 12;
+
+//normal function
+function add(a: number, b: number): number {
+    return a + b;
+}
+console.log(add(1, 5));
+
+//function expression
+const add2 = function(a: number, b: number): number {
+    return a + b;
+}
+console.log(add2(4, 8));
+
+//arrow function
+const add3 = (a: number, b: number): number => {
+    return a + b;
+}
+console.log(add3(4, 6));
