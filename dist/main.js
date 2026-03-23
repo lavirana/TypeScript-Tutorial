6ca8524e7b37480525bb5daae98811522e594b54
Ashish Rana <ashishrana288@gmail.com>
2026-03-23 17:36:02 +0530

    added function overloading code

--- dist/main.js
+++ dist/main.js
@@ -21,4 +21,32 @@ const add3 = (a, b) => {
     return a + b;
 };
 console.log(add3(4, 6));
+//implicit return in arrow function
+const add4 = (a, b) => a + b;
+console.log(add4(7, 7));
+function great(name) {
+    console.log(`Hello ${name}`);
+}
+great('Ashish Rana');
+function great2(name) {
+    console.log(`Hello ${name !== null && name !== void 0 ? name : "guest"}`);
+}
+great2();
+//throw error
+//infinite loop
+function throwError(message) {
+    throw new Error(message);
+}
+throwError('Custom Error Message');
+function combine(a, b) {
+    if (typeof a === "number" && typeof b === "number") {
+        return a + b;
+    }
+    else if (typeof a === "string" && typeof b === "string") {
+        return a + " " + b;
+    }
+    else {
+        throwError("invalid args");
+    }
+}
 export {};
--- src/main.ts
+++ src/main.ts
@@ -26,3 +26,39 @@ const add3 = (a: number, b: number): number => {
     return a + b;
 }
 console.log(add3(4, 6));
+
+//implicit return in arrow function
+const add4 = (a: number, b: number): number => a + b;
+console.log(add4(7, 7));
+
+function great(name: string): void {
+    console.log(`Hello ${name}`);
+}
+great('Ashish Rana');
+
+function great2(name?: string): void {
+    console.log(`Hello ${name ?? "guest"}`);
+}
+great2();
+
+//throw error
+//infinite loop
+
+function throwError(message: string): never {
+    throw new Error(message)
+}
+throwError('Custom Error Message');
+
+// function overloading
+function combine(a: number, b: number): number;
+function combine(a: string, b: string): string;
+
+function combine(a: number | string, b: number | string): string | number {
+    if(typeof a === "number" && typeof b === "number") {
+        return a + b;
+    }else if(typeof a === "string" && typeof b === "string"){
+        return a + " " + b;
+    }else{
+        throwError("invalid args");
+    }
+}
